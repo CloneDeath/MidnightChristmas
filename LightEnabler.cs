7b59d23f3712dd60bcd4ef0ed2011c229947921c
Nicholas Rodine <halfofastaple@gmail.com>
2022-11-27 07:37:58 -0500

    Started work on a new level

---
+++ LightEnabler.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System.Linq;
+using Godot;
+
+namespace MidnightChristmas; 
+
+public partial class LightEnabler : Node3D
+{
+	public override void _Process(double delta) {
+		var lights = GetLightsInScene()
+			.OrderBy(l => l.GlobalPosition.DistanceTo(GlobalPosition))
+			.ToList();
+
+		const int max_lights = 7;
+		var lightsOn = lights.Take(max_lights);
+		foreach (var light in lightsOn) {
+			light.Visible = true;
+		}
+
+		var lightsOff = lights.Skip(max_lights);
+		foreach (var light in lightsOff) {
+			light.Visible = false;
+		}
+	}
+
+	private IEnumerable<Light3D> GetLightsInScene() {
+		return GetTree().GetNodesInGroup("light").ToArray().Cast<Light3D>();
+	}
+}
